9a70f8ae4a94e4e451f5e9d954cb5b517f07e538
Teppei-tt <teppei.takata03@gmail.com>
2022-12-02 23:32:07 +0900

    ⚗️ クリックイベントの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,26 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../componets/Footer";
 import { Main } from "../componets/Main";
 import { Header } from "../componets/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  });
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
