a02d9ba5a64edcaa69ef26380d3c312efba87e68
Teppei-tt <teppei.takata03@gmail.com>
2022-12-03 20:41:01 +0900

    useEffeectの確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,24 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../componets/Footer";
 import { Main } from "../componets/Main";
 import { Header } from "../componets/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
-  const foo = 1;
+  // const foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  });
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();
+  //   alert(foo);
+  // });
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -20,9 +28,9 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </a> */}
       <Main page="index" />
       <Footer />
     </div>
